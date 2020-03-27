29731f1b5100a564fb35f46e0d690dd91b6d8296
Baran Önen <baranonenn@gmail.com>
2020-03-28 01:22:51 +0300

    Header added

--- fasttake/App.js
+++ fasttake/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import Header from './src/header';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <Header/>
       <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -13,7 +15,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
