ab5911676d62709346696fc6de1e6474ff28f95c
Arda Çebi <ardacebi1@gmail.com>
2020-03-29 19:19:55 +0300

    start settings page

--- lib/settings.dart
+++ lib/settings.dart
@@ -8,20 +8,55 @@ class SettingsRoute extends StatefulWidget {
 class _SettingsRouteState extends State<SettingsRoute> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-
-      children: <Widget>[
-              Container(
-                margin: const EdgeInsets.only(top: 60.0),
-                child: Text(
-                  'settings',
-                  style: TextStyle(fontFamily: 'JosefinSans', fontSize: 50),
-                ),
-              ),
-      ],
+    return new Scaffold(
+      appBar: new AppBar(
+          backgroundColor: Colors.transparent,
+          bottomOpacity: 0.0,
+          elevation: 0.0,
+          title: new Text('Settings',
+              style: TextStyle(fontWeight: FontWeight.w300, fontSize: 30.0)),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              tooltip: "Search",
+              onPressed: () {},
+            ),
+          ]),
+      body: new ListView(
+        children: ListTile.divideTiles(
+          context: context,
+          tiles: [
+            ListTile(
+              leading: Icon(Icons.notifications),
+              title: Text('Notifications'),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              subtitle: Text('Manage your push notification settings'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.notifications),
+              title: Text('Notifications'),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              subtitle: Text('Manage your push notification settings'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.notifications),
+              title: Text('Notifications'),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              subtitle: Text('Manage your push notification settings'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.notifications),
+              title: Text('Notifications'),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              subtitle: Text('Manage your push notification settings'),
+              onTap: () {},
+            ),
+          ],
+        ).toList(),
       ),
-      
     );
   }
 }
