3849b0ff513e89499f70b26f4a1df62462a0101d
Arda Çebi <ardacebi1@gmail.com>
2020-03-30 15:46:44 +0300

    about page contents initialization

--- lib/settings/item_about.dart
+++ lib/settings/item_about.dart
@@ -1,21 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:package_info/package_info.dart';
+import 'dart:async';
 
 class SettingsItemAbout extends StatefulWidget {
+  SettingsItemAbout({Key key, this.title}) : super(key: key);
+
+  final String title;
+
   @override
   _SettingsItemAboutState createState() => _SettingsItemAboutState();
 }
 
 class _SettingsItemAboutState extends State<SettingsItemAbout> {
+  PackageInfo _packageInfo = PackageInfo(
+    appName: 'Unknown',
+    packageName: 'Unknown',
+    version: 'Unknown',
+    buildNumber: 'Unknown',
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _initPackageInfo();
+  }
+
+  Future<void> _initPackageInfo() async {
+    final PackageInfo info = await PackageInfo.fromPlatform();
+    setState(() {
+      _packageInfo = info;
+    });
+  }
+
+  Widget _infoTile(String title, String subtitle) {
+    return ListTile(
+      title: Text(title),
+      subtitle: Text(subtitle ?? 'Not set'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         leading: new IconButton(
-            icon: new Icon(SimpleLineIcons.arrow_left, color: Colors.black),
-            onPressed: () => Navigator.of(context).pop(),
-          ),
+          icon: new Icon(SimpleLineIcons.arrow_left, color: Colors.black),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
         backgroundColor: Colors.transparent,
         bottomOpacity: 0.0,
         elevation: 0.0,
@@ -23,18 +56,24 @@ class _SettingsItemAboutState extends State<SettingsItemAbout> {
         title: Column(
           children: [
             Text(
-              "about", style: TextStyle(fontWeight: FontWeight.w300, fontSize: 30.0),
+              "about",
+              style: TextStyle(fontWeight: FontWeight.w300, fontSize: 30.0),
             ),
             GestureDetector(
-              child: Text('settings', style: TextStyle(fontWeight: FontWeight.w300, fontSize: 20.0)),
+              child: Text('settings',
+                  style:
+                      TextStyle(fontWeight: FontWeight.w300, fontSize: 20.0)),
               onTap: () {},
             ),
           ],
         ),
       ),
-      body: SingleChildScrollView(
-        
-       
+      body: Column(
+        children: <Widget>[
+          _infoTile('app version', _packageInfo.version),
+          _infoTile('build number', _packageInfo.buildNumber),
+          _infoTile('channel', 'stable'),
+        ],
       ),
     );
   }
