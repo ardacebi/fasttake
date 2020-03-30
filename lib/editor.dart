1f863b2b4ea21b67dbf7f0b391997113a33357bb
Arda Çebi <ardacebi1@gmail.com>
2020-03-30 16:11:16 +0300

    replace new take route with markdown editor

---
+++ lib/editor.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:quill_delta/quill_delta.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:zefyr/zefyr.dart';
+
+class EditorRoute extends StatefulWidget {
+  @override
+  EditorRouteState createState() => EditorRouteState();
+}
+
+class EditorRouteState extends State<EditorRoute> {
+  /// Allows to control the editor and the document.
+  ZefyrController _controller;
+
+  /// Zefyr editor like any other input field requires a focus node.
+  FocusNode _focusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    // Here we must load the document and pass it to Zefyr controller.
+    final document = _loadDocument();
+    _controller = ZefyrController(document);
+    _focusNode = FocusNode();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Note that the editor requires special `ZefyrScaffold` widget to be
+    // one of its parents.
+    return Scaffold(
+      appBar: AppBar(
+        
+        leading: new IconButton(
+          icon: new Icon(SimpleLineIcons.arrow_left, color: Colors.black),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        title: new Text('new take',
+            style: TextStyle(
+                fontWeight: FontWeight.w300,
+                fontSize: 30.0,
+                fontFamily: 'JosefinSans')),
+        backgroundColor: Colors.transparent,
+        bottomOpacity: 0.0,
+        elevation: 0.0,
+        centerTitle: true,
+        actions: <Widget>[
+        FlatButton(
+          child: Text("done", style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.w300, fontFamily: 'JosefinSans')),
+          onPressed: () {},
+        ),
+      ],
+      ),
+      body: ZefyrScaffold(
+        child: ZefyrEditor(
+          padding: EdgeInsets.all(16),
+          controller: _controller,
+          focusNode: _focusNode,
+        ),
+      ),
+    );
+  }
+
+  /// Loads the document to be edited in Zefyr.
+  NotusDocument _loadDocument() {
+    // For simplicity we hardcode a simple document with one line of text
+    // saying "Zefyr Quick Start".
+    // (Note that delta must always end with newline.)
+    final Delta delta = Delta()..insert("\n");
+    return NotusDocument.fromDelta(delta);
+  }
+}
