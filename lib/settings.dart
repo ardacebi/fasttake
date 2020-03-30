337d7093a1b9546169e922dc60428b6b90c91b91
Arda Çebi <ardacebi1@gmail.com>
2020-03-30 12:04:03 +0300

    replace all material icons with simplelineicons

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 
 class SettingsRoute extends StatefulWidget {
   @override
@@ -10,18 +11,20 @@ class _SettingsRouteState extends State<SettingsRoute> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
+          leading: new IconButton(
+            icon: new Icon(SimpleLineIcons.arrow_left, color: Colors.black),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
           backgroundColor: Colors.transparent,
           bottomOpacity: 0.0,
           elevation: 0.0,
           centerTitle: true,
           title: new Text('settings',
-              style: TextStyle(fontWeight: FontWeight.w300, fontSize: 30.0, fontFamily: 'JosefinSans')),
+              style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                  fontSize: 30.0,
+                  fontFamily: 'JosefinSans')),
           actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.search),
-              tooltip: "Search",
-              onPressed: () {},
-            ),
           ]),
       body: new ListView(
         children: ListTile.divideTiles(
@@ -29,9 +32,20 @@ class _SettingsRouteState extends State<SettingsRoute> {
           tiles: [
             ListTile(
               leading: Icon(Icons.notifications),
-              title: Text('Notifications', style: TextStyle(fontFamily: 'JosefinSans')),
-              trailing: Icon(Icons.keyboard_arrow_right),
-              subtitle: Text('Manage your push notification settings', style: TextStyle(fontFamily: 'JosefinSans')),
+              title: Text('Notifications',
+                  style: TextStyle(fontFamily: 'JosefinSans')),
+              trailing: Icon(SimpleLineIcons.arrow_right),
+              subtitle: Text('Manage your push notification settings',
+                  style: TextStyle(fontFamily: 'JosefinSans')),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.notifications),
+              title: Text('Notifications',
+                  style: TextStyle(fontFamily: 'JosefinSans')),
+              trailing: Icon(SimpleLineIcons.arrow_right),
+              subtitle: Text('Manage your push notification settings',
+                  style: TextStyle(fontFamily: 'JosefinSans')),
               onTap: () {},
             ),
           ],
