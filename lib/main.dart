f67c569a56808b2a2730dda6e901b64cb716e55f
Arda Çebi <ardacebi1@gmail.com>
2020-04-01 18:14:58 +0300

    theme enhancements: fix take card color issue

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@ import 'package:sqflite/sqflite.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,20 +25,13 @@ class MyApp extends StatelessWidget {
         highlightColor: Colors.transparent,
         primarySwatch: Colors.grey,
         accentColor: Colors.grey,
+        cardColor: Colors.grey[850],
         fontFamily: 'JosefinSans',
       ),
       theme: ThemeData(
         splashColor: Colors.transparent,
+        cardColor: Colors.grey[50],
         highlightColor: Colors.transparent,
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.grey,
         fontFamily: 'JosefinSans',
       ),
