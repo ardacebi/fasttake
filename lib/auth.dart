9cf521e64172dd90275fcc4fc1fd7b4cda02d9eb
Arda Çebi <ardacebi1@gmail.com>
2020-04-04 17:46:39 +0300

    Separate firebase auth to class and file

---
+++ lib/auth.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:async';
+
+abstract class BaseAuth {
+  Future<String> signInWithEmailAndPassword(String email, String password);
+  Future<String> createUserWithEmailAndPassword(String email, String password);
+}
+
+class Auth implements BaseAuth {
+  Future<String> signInWithEmailAndPassword(
+      String email, String password) async {
+    FirebaseUser user = (await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email, password: password)) as FirebaseUser;
+    return user.uid;
+  }
+
+  Future<String> createUserWithEmailAndPassword(
+      String email, String password) async {
+    FirebaseUser user = (await FirebaseAuth.instance
+            .createUserWithEmailAndPassword(email: email, password: password))
+        as FirebaseUser;
+    return user.uid;
+  }
+}
