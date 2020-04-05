29d9f52d7d00474b0133748d495fd6c9a626262a
Arda Çebi <ardacebi1@gmail.com>
2020-04-05 16:24:03 +0300

    Closes #17

--- lib/settings/item_feedback.dart
+++ lib/settings/item_feedback.dart
@@ -4,15 +4,53 @@ import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_translate/flutter_translate.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:uuid/uuid.dart';
+import 'dart:math';
 
 class SettingsItemFeedback extends StatefulWidget {
   @override
   _SettingsItemFeedbackState createState() => _SettingsItemFeedbackState();
 }
 
+var uuid = Uuid();
+
 class _SettingsItemFeedbackState extends State<SettingsItemFeedback> {
+  final fb = FirebaseDatabase.instance;
+  final myController = TextEditingController();
+  final feedback = "feedback/" + uuid.v1();
+
   @override
   Widget build(BuildContext context) {
+    Future<void> _neverSatisfied() async {
+      return showDialog<void>(
+        context: context,
+        barrierDismissible: true,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Feedback Submitted'),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  Text('Your feedback has been submitted.'),
+                  Text('Thank you for your help.'),
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('DISMISS'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+
+    final ref = fb.reference();
     return new Scaffold(
       appBar: new AppBar(
         leading: new IconButton(
@@ -42,17 +80,16 @@ class _SettingsItemFeedbackState extends State<SettingsItemFeedback> {
         children: <Widget>[
           Padding(
               padding: EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
-              child: Text(
-                  translate('settings_feedback.items.description1'))),
+              child: Text(translate('settings_feedback.items.description1'))),
           Padding(
               padding: EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
-              child: Text(
-                  translate('settings_feedback.items.description2'))),
+              child: Text(translate('settings_feedback.items.description2'))),
           Padding(
             padding: EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
             child: Container(
               margin: const EdgeInsets.only(bottom: 20.0),
               child: TextField(
+                controller: myController,
                 maxLines: null,
                 minLines: 3,
                 textCapitalization: TextCapitalization.sentences,
@@ -64,7 +101,7 @@ class _SettingsItemFeedbackState extends State<SettingsItemFeedback> {
               ),
             ),
           ),
-          new Padding(
+          /*new Padding(
               padding: new EdgeInsets.only(top: 0.0, bottom:15.0, left:60.0, right:60.0),
               child: ConstrainedBox(
                 constraints: const BoxConstraints(minWidth: double.infinity),
@@ -76,20 +113,30 @@ class _SettingsItemFeedbackState extends State<SettingsItemFeedback> {
                   },
                 ),
               ),
-            ),
+            ),*/
           Padding(
-            padding: EdgeInsets.only(left:20.0, right:20.0),
+            padding: EdgeInsets.only(left: 20.0, right: 20.0),
             child: ConstrainedBox(
-                constraints: const BoxConstraints(minWidth: double.infinity),
-                child: RaisedButton(
-                  child: Text(translate('settings_feedback.items.send_feedback')),
-                  onPressed: () {},
-                ),
+              constraints: const BoxConstraints(minWidth: double.infinity),
+              child: RaisedButton(
+                child: Text(translate('settings_feedback.items.send_feedback')),
+                onPressed: () {
+                  ref.child(feedback).set(myController.text);
+                  Navigator.pop(context);
+                  _neverSatisfied();
+                },
               ),
+            ),
           ),
-          
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
   }
 }
