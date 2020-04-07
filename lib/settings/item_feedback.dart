c0215490b729ab670a09fcabd0478ae105de44b3
Arda Çebi <ardacebi1@gmail.com>
2020-04-07 10:00:12 +0300

    Convert to regular button (temp)

--- lib/settings/item_feedback.dart
+++ lib/settings/item_feedback.dart
@@ -106,6 +106,7 @@ class _SettingsItemFeedbackState extends State<SettingsItemFeedback> {
               ),
             ),
           ),
+          
           /*new Padding(
               padding: new EdgeInsets.only(top: 0.0, bottom:15.0, left:60.0, right:60.0),
               child: ConstrainedBox(
@@ -123,7 +124,7 @@ class _SettingsItemFeedbackState extends State<SettingsItemFeedback> {
             padding: EdgeInsets.only(left: 20.0, right: 20.0),
             child: Column( 
                children: <Widget>[
-            fastButton(
+            RaisedButton(
               onPressed: () {
                   setState(() {
                     feedbackFieldController.text.isEmpty
